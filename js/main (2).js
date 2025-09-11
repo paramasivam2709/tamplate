14c40d5af498d12f8e0d0636e5b4337466c915a8
paramasivam2709 <paramasivamb2000@gmail.com>
2025-09-11 14:12:27 +0530

    Add files via upload

--- js/main (2).js
+++ js/main (2).js
@@ -1,14 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
+    
 
 
     // Smooth scrolling on the navbar links
